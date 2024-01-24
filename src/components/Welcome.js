f1eb126cf1ad1235408b0bc9d5aeb0f684431892
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-24 17:13:28 +0545

    adsf

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 
 class Welcome extends Component {
   render() {
-    return <h1>Class Component</h1>
+    return <h1>Class Component {props.name} a.k.a {props.alias}</h1>
   }
 }
 
