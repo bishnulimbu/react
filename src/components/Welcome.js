e19048c4bd7f8b8edb454d70fa103e858bb3d892
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-24 17:21:09 +0545

    dfd

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 
 class Welcome extends Component {
   render() {
-    return <h1>Class Component {this.props.name} a.k.a {this.props.alias} also {this.probs.children}</h1>
+    return <h1>Class Component {this.props.name} a.k.a {this.props.alias}</h1>
   }
 }
 //class comonents have predefined props keyword and this is used for variable
