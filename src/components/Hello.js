acd294965abe9c6dac789b8118dddcce86a29089
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-24 16:58:37 +0545

    dsf

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const Hello = () => {
-  // return (
-  //   <div>
-  //     <h1>hellow new line</h1>
-  //
-  //   </div>
-  // )
-  return React.createElement(
-    'div',
-    { id: 'hellow', className: 'dummyclasse' },
-    React.createElement('h1', null, 'hello'))
+  return (
+    <div>
+      <h1>hellow new line</h1>
+
+    </div>
+  )
 }
+// return React.createElement(
+//   'div',
+//   { id: 'hellow', className: 'dummyclasse' },
+//   React.createElement('h1', null, 'hello'))
+// }
 export default Hello
