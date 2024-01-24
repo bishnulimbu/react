bd06e4a6c9a48a56fe773e1a63140fe555b7b7a4
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-24 16:54:37 +0545

    dsafs

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet() {
+//   return <h1>Hellow world</h1>
+//
+
+// }
+export const MyComponent = () =>
+  <h1>hellow world</h1>
+
+
+// export default Greet 
