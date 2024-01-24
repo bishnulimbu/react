6a8b12c87af865563e68c2529ce6e5d9567e6fc9
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-24 17:15:21 +0545

    dff

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,5 +6,5 @@ class Welcome extends Component {
     return <h1>Class Component {this.props.name} a.k.a {this.props.alias}</h1>
   }
 }
-
+//class comonents have predefined props keyword and this is used for variable
 export default Welcome
