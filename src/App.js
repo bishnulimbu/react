443739a8c7e5ce36310f875137666af27eeb30ab
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-25 12:50:14 +0545

    df

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import { MyComponent } from './components/Greet'
 import Welcome from './components/Welcome.js'
 import Hello from './components/Hello.js'
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <MyComponent name="ram" />
-      <MyComponent name="sam" />
-      <MyComponent name="hari" />
-      <MyComponent name="dum" >this is the children prop</MyComponent>
-      <Welcome name="hello" alias="world" />
-      <Hello name="kalay" alias="goray" />
+      {/* <MyComponent name="ram" /> */}
+      {/* <MyComponent name="sam" /> */}
+      {/* <MyComponent name="hari" /> */}
+      {/* <MyComponent name="dum" >this is the children prop</MyComponent> */}
+      {/* <Welcome name="hello" alias="world" /> */}
+      {/* <Hello name="kalay" alias="goray" /> */}
+      <Message />
 
     </div>
   );
