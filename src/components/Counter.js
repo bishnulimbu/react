6134f112cabf63a5c89f87e3cfaf2fc9950b8c9e
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-25 13:12:55 +0545

    cdfd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,1 +1,18 @@
+import React, { Component } from 'react'
 
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+  render() {
+    return (
+      <div>count</div>
+    )
+  }
+}
+
+export default Counter 
